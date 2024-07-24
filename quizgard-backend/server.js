d3d12cf1429c582738bd3ecda92d49c1cb3a509d
Ob55 <brian55mwangi@gmail.com>
2024-07-24 13:12:07 +0300

    done

--- quizgard-backend/server.js
+++ quizgard-backend/server.js
@@ -12,11 +12,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('Failed to connect to MongoDB', err);
